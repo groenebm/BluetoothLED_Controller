51192e7da21ad27af8df12cb530fc65004cde5e5
Brandt Groene <groenebm@miamioh.edu>
2018-05-11 23:40:06 -0400

    Add files via upload

---
+++ EMD_Final.ino
@@ -0,0 +1,179 @@
+#define REDPIN 5
+#define BLUEPIN 3
+#define GREENPIN 6
+#include "pitches.h"
+int Received=0;
+char Rec = 's'; 
+#define FADESPEED 2     // make this higher to slow down
+#define melodyPin 9
+
+
+
+
+int wish_melody[] = {
+  NOTE_B3, 
+  NOTE_F4, NOTE_F4, NOTE_G4, NOTE_F4, NOTE_E4,
+  NOTE_D4, NOTE_D4, NOTE_D4,
+  NOTE_G4, NOTE_G4, NOTE_A4, NOTE_G4, NOTE_F4,
+  NOTE_E4, NOTE_E4, NOTE_E4,
+  NOTE_A4, NOTE_A4, NOTE_B4, NOTE_A4, NOTE_G4,
+  NOTE_F4, NOTE_D4, NOTE_B3, NOTE_B3,
+  NOTE_D4, NOTE_G4, NOTE_E4,
+  NOTE_F4
+};
+
+int wish_tempo[] = {
+  4,
+  4, 8, 8, 8, 8,
+  4, 4, 4,
+  4, 8, 8, 8, 8,
+  4, 4, 4,
+  4, 8, 8, 8, 8,
+  4, 4, 8, 8,
+  4, 4, 4,
+  2
+};
+ 
+void setup() {
+  Serial.begin(9600);
+  pinMode(REDPIN, OUTPUT);
+  pinMode(GREENPIN, OUTPUT);
+  pinMode(BLUEPIN, OUTPUT);
+  pinMode(9, OUTPUT);
+}
+ 
+ 
+void loop() {
+  int r, g, b;
+ 
+ if(Serial.available()>0)
+ { 
+    
+    Received = Serial.read();
+    Rec = char(Received);
+    if (Rec != '0')
+    {
+    Serial.println(Rec); 
+    Serial.println("working");
+    }
+    
+ }  if(Rec == 'c') { 
+    sing();
+ 
+  } 
+ 
+
+  if(Rec == 'r') { 
+    analogWrite(REDPIN, 255);
+ 
+  } 
+  if(Rec == 'b') { 
+    analogWrite(BLUEPIN, 255);
+ 
+  } 
+  if(Rec == 'g') { 
+    analogWrite(GREENPIN, 255);
+ 
+  }
+  if(Rec == 'k') {  // fade from blue to violet
+  for (r = 0; r < 256; r++) { 
+    analogWrite(REDPIN, r);
+    delay(FADESPEED);
+  } 
+  // fade from violet to red
+  for (b = 255; b > 0; b--) { 
+    analogWrite(BLUEPIN, b);
+    delay(FADESPEED);
+  } 
+  // fade from red to yellow
+  for (g = 0; g < 256; g++) { 
+    analogWrite(GREENPIN, g);
+    delay(FADESPEED);
+  } 
+  // fade from yellow to green
+  for (r = 255; r > 0; r--) { 
+    analogWrite(REDPIN, r);
+    delay(FADESPEED);
+  } 
+  // fade from green to teal
+  for (b = 0; b < 256; b++) { 
+    analogWrite(BLUEPIN, b);
+    delay(FADESPEED);
+  } 
+  // fade from teal to blue
+  for (g = 255; g > 0; g--) { 
+    analogWrite(GREENPIN, g);
+    delay(FADESPEED);
+  } }else {
+   analogWrite(GREENPIN, 10);
+   analogWrite(BLUEPIN, 10);
+   analogWrite(REDPIN, 10);
+
+}
+}
+void sing() {
+    Serial.println(" 'We wish you a Merry Christmas'");
+    int size = sizeof(wish_melody) / sizeof(int);
+    for (int thisNote = 0; thisNote < size; thisNote++) {
+    if(thisNote % 2 == 0) {analogWrite(GREENPIN, 255); analogWrite(REDPIN,0);}
+    else {analogWrite(GREENPIN, 0); analogWrite(REDPIN,255);}
+      // to calculate the note duration, take one second
+      // divided by the note type.
+      //e.g. quarter note = 1000 / 4, eighth note = 1000/8, etc.
+      int noteDuration = 1000 / wish_tempo[thisNote];
+
+      buzz(melodyPin, wish_melody[thisNote], noteDuration);
+
+      // to distinguish the notes, set a minimum time between them.
+      // the note's duration + 30% seems to work well:
+      int pauseBetweenNotes = noteDuration * 1.30;
+      delay(pauseBetweenNotes);
+
+      // stop the tone playing:
+      buzz(melodyPin, 0, noteDuration);
+
+    }
+  
+  }
+
+
+
+int tempo = 300;
+void playTone(int tone, int duration) {
+  for (long i = 0; i < duration * 1000L; i += tone * 2) {
+    digitalWrite(9, HIGH);
+    delayMicroseconds(tone);
+    digitalWrite(9, LOW);
+    delayMicroseconds(tone);
+  }
+}
+void playNote(char note, int duration) {
+  char names[] = { 'c', 'd', 'e', 'f', 'g', 'a', 'b', 'C' };
+  int tones[] = { 1915, 1700, 1519, 1432, 1275, 1136, 1014, 956 };
+  
+  // play the tone corresponding to the note name
+  for (int i = 0; i < 8; i++) {
+    if (names[i] == note) {
+      playTone(tones[i], duration);
+    }
+  }
+}
+
+void buzz(int targetPin, long frequency, long length) {
+  digitalWrite(13, HIGH);
+  long delayValue = 1000000 / frequency / 2; // calculate the delay value between transitions
+  //// 1 second's worth of microseconds, divided by the frequency, then split in half since
+  //// there are two phases to each cycle
+  long numCycles = frequency * length / 1000; // calculate the number of cycles for proper timing
+  //// multiply frequency, which is really cycles per second, by the number of seconds to
+  //// get the total number of cycles to produce
+  for (long i = 0; i < numCycles; i++) { // for the calculated length of time...
+    digitalWrite(targetPin, HIGH); // write the buzzer pin high to push out the diaphram
+    delayMicroseconds(delayValue); // wait for the calculated delay value
+    digitalWrite(targetPin, LOW); // write the buzzer pin low to pull back the diaphram
+    delayMicroseconds(delayValue); // wait again or the calculated delay value
+  }
+  digitalWrite(13, LOW);
+
+}
+
